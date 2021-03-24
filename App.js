c4b064db05208f1fa85a0a0a6b93cc992ecce57b
niksankarkee <karkeeniksan@gmail.com>
2021-03-24 22:32:24 +0900

    added some style header and box

--- App.js
+++ App.js
@@ -1,55 +1,20 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Button, FlatList } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-import GoalItem from './components/GoalItem';
-import GoalInput from './components/GoalInput';
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
-  const [courseGoals, setCourseGoals] = useState([]);
-  const [isAddMode, setIsAddMode] = useState(false);
-
-  const addGoalHandler = (goalTitle) => {
-    setCourseGoals((currentGoals) => [
-      ...currentGoals,
-      { id: Math.random().toString(), value: goalTitle },
-    ]);
-    setIsAddMode(false);
-  };
-
-  const removeGoalHandler = (goalId) => {
-    setCourseGoals((currentGoals) => {
-      return currentGoals.filter((goal) => goal.id !== goalId);
-    });
-  };
-
-  const cancleGoalAdditionHandler = () => {
-    setIsAddMode(false);
-  };
-
   return (
     <View style={styles.screen}>
-      <Button title='Add New Goal' onPress={() => setIsAddMode(true)} />
-      <GoalInput
-        visible={isAddMode}
-        onAddGoal={addGoalHandler}
-        onCancle={cancleGoalAdditionHandler}
-      />
-      <FlatList
-        keyExtractor={(item, index) => item.id}
-        data={courseGoals}
-        renderItem={(itemData) => (
-          <GoalItem
-            onDelete={removeGoalHandler.bind(this, itemData.item.id)}
-            title={itemData.item.value}
-          />
-        )}
-      />
+      <Header title='Guess a Number' />
+      <StartGameScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 50,
+    flex: 1,
   },
 });
